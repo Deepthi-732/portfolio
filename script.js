6564e7a7def600a7def8c30bd25fc3338054b554
Deepthi-732 <139119979+Deepthi-732@users.noreply.github.com>
2023-07-12 10:35:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Azure Architect", "Cloud Enginner"],
+        strings: ["Engineer", "Cloud Architect"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Cloud Expert, J2EE Expert"],
+        strings: ["Problem solver , Code enthusiast"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
